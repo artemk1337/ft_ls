a33e63199320c27fa939b18241894ecf57207e0f
cchadwic <artemkush1@gmail.com>
2020-02-02 22:51:01 +0000

    fix

--- src/main.c
+++ src/main.c
@@ -329,7 +329,7 @@ t_files		*sort_files(t_ls *ls, t_files *curr_f, t_path *curr_d, int size)
 	revive(curr_d, &arr, ls, size - 1);
 
 	i = -1;
-	if (ls-r == 0)
+	if (ls->r == 0)
 	{
 		new_f = arr[i];
 		while (arr[++i])
