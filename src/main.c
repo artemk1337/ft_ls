6d61e42f83d82f13f3683e10b4f3de3d1c74d898
cchadwic <artemkush1@gmail.com>
2020-02-02 23:19:07 +0000

    fix

--- src/main.c
+++ src/main.c
@@ -217,16 +217,16 @@ int			file_hide(char *file)
 
 
 
-void		reverse_arr(t_files **arr, int max)
+void		reverse_arr(t_files ***arr, int max)
 {
 	int		i;
 	t_files	*tmp;
 
 	while (i < max)
 	{
-		tmp = arr[i];
-		arr[i] = arr[max - i];
-		arr[max - i] = tmp;
+		tmp = arr[0][i];
+		arr[0][i] = arr[0][max - i];
+		arr[0][max - i] = tmp;
 		i++;
 	}
 
@@ -257,7 +257,7 @@ t_files		*sort_files(t_ls *ls, t_files *start, int max)
 	}
 
 
-	reverse_arr(arr_f, max);
+	reverse_arr(&arr_f, max);
 	i = 0;
 	while (i < max)
 		printf("%s\n", arr_f[i++]->filename);
